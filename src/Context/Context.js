514aab3f21feb215fb90e809ce49870b87dbcd64
jin sun kyoung <jinsk9268@gmail.com>
2020-10-08 02:31:22 +0900

    Add 모바일 nav modal 1차 구현

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,17 +1,22 @@
 import React, { useState, createContext, memo } from 'react';
-
+import MobileNavModal from 'Components/MobileNavModal';
 export const MenuContext = createContext(null);
+export const MobileNavContext = createContext(null);
 export const TokenContext = createContext(null);
 
 const ContextProvider = (props) => {
-  const [token, setToken] = useState('');
   const [menu, setMenu] = useState('');
+  const [navModalShow, setNavModalShow] = useState(false);
+  const [token, setToken] = useState('');
 
   return (
     <MenuContext.Provider value={[menu, setMenu]}>
-      <TokenContext.Provider value={[token, setToken]}>
-        {props.children}
-      </TokenContext.Provider>
+      <MobileNavContext.Provider value={[navModalShow, setNavModalShow]}>
+        <TokenContext.Provider value={[token, setToken]}>
+          {props.children}
+          {navModalShow && <MobileNavModal />}
+        </TokenContext.Provider>
+      </MobileNavContext.Provider>
     </MenuContext.Provider>
   );
 };
